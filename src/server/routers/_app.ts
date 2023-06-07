61a037e2a0d0f910c56abee59858d591a89563c3
Nick West <nick@glaziersoftware.com>
2023-06-07 15:33:32 -0400

    tab to space

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -8,14 +8,15 @@ import * as trpc from '@trpc/server';
 import { transformer } from '~/utils/transformer';
 
 export const legacyRouter = trpc
-	.router()
-   // With transformer, procedures fail to run as there are "two transformers"
-   // .transformer(transformer)
-	.query('health', {
-		resolve() {
-			return 'yay!';
-		},
-	}).interop();
+  .router()
+  // With transformer, procedures fail to run as there are "two transformers"
+  // .transformer(transformer)
+  .query('health', {
+    resolve() {
+      return 'yay!';
+    },
+  })
+  .interop();
 
 export const mainRouter = router({
   healthcheck: publicProcedure.query(() => 'yay!'),
@@ -24,6 +25,5 @@ export const mainRouter = router({
 });
 
 export const appRouter = mergeRouters(legacyRouter, mainRouter);
-
 
 export type AppRouter = typeof appRouter;
